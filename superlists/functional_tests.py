bcef6a569f18958e3505f99fbedf7af8bcaaa6c4
Magdalena Zielinska <zielinska.magdalen@gmail.com>
2020-08-31 11:30:53 +0200

    Reduce repetitions in the FT

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -12,7 +12,12 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser = webdriver.Firefox()
 
 	def tearDown(self):
-		self.browser.quit() 
+		self.browser.quit()
+
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows]) 
 	
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith has heard about a cool new online to-do app, 
@@ -40,11 +45,7 @@ class NewVisitorTest(unittest.TestCase):
 		# 1: Buy peacock feathers to make a fly
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
-		
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peakock feathers', [row.text for row in rows])
-
+		self.check_for_row_in_list_table('1: Buy peakock feathers')
 		
 		# There is still a text box inviting her to add anothher item. She
 		# enters 'Use peacock feathers to make a fly (She is very methodical)'
@@ -54,11 +55,8 @@ class NewVisitorTest(unittest.TestCase):
 		time.sleep(1)
 
 		# The page updates again, and now shows both items on her list
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peakock feathers', [row.text for row in rows])
-		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
-
+		self.check_for_row_in_list_table('1: Buy peakock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 # Edith wonders whether the site will remember thhe list. Then she see that the site
 #has generated a unique URL for her -- there is some expalanatory text to that effect
